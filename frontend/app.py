e969df221f23eedcbd812b20a4319ee9202c6712
Zia Uddin Chowdhury <rifatxia9@gmail.com>
2023-07-20 21:03:31 +0600

    frontend updated

--- frontend/app.py
+++ frontend/app.py
@@ -3,13 +3,6 @@ from streamlit import components
 
 # Set the page title
 
-with open("CSE499-Project\\frontend\index.html", "r") as file:
+with open("../frontend/index.html", "r") as file:
     html_content = file.read()
-    st.write(html_content, height=800, scrolling=True)
-
-
-
-
-
-
-
+    st.write(html_content, unsafe_allow_html=True)
