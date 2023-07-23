225321cd2b81310da9cb7401eb580ad5715eaeca
Nafisa Prome <aymannafisapromi390@gmail.com>
2023-07-23 13:28:26 +0600

    icons and logo install

--- frontend/app.py
+++ frontend/app.py
@@ -1,8 +1,20 @@
-import streamlit as st
-from streamlit import components
+# import streamlit as st
+# from streamlit import components
 
-# Set the page title
+# # Set the file path
+# file_path = r"C:\\Users\ASUS\Desktop\\CSE499 Project\\CSE499-Project\\frontend\\index.html"
 
-with open("../frontend/index.html", "r") as file:
-    html_content = file.read()
-    st.write(html_content, unsafe_allow_html=True)
+# # Read the "index.html" file containing the HTML content
+# with open(file_path, "r", encoding='utf-8') as file:
+#     html_content = file.read()
+
+# # Display the HTML content in Streamlit
+# st.components.v1.html(html_content, height=200)
+
+# with open(r"C:\\Users\ASUS\Desktop\\CSE499 Project\\CSE499-Project\\frontend\style.css", "r") as file:
+#     css_content = file.read()
+
+# # Display the CSS content using the style tag within a markdown block
+# st.markdown(r'<link rel="stylesheet" href="C:\\Users\ASUS\Desktop\\CSE499 Project\\CSE499-Project\\frontend\styles.css">', unsafe_allow_html=True)
+
+
