34bd86881f1867e20ec8fe2b2fa82688d4453e61
Zia Uddin Chowdhury <rifatxia9@gmail.com>
2023-07-24 22:46:42 +0600

    navigator updated

--- frontend/script.js
+++ frontend/script.js
@@ -3,5 +3,5 @@ let mynav = document.querySelector(".navbar")
 
 menubar.onclick = () => {
     menubar.classList.toggle('fa-times')
-    // mynav.classList.toggle("active")
+    mynav.classList.toggle("active")
 }
