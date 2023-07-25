e9e2f56f5dbc52154307e49e8c0cc56e5231c8dc
Zia Uddin Chowdhury <rifatxia9@gmail.com>
2023-07-26 03:14:48 +0600

    App updated

--- CooperHospital/App.js
+++ CooperHospital/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Button, StyleSheet, Text, View, SafeAreaView, FlatList, ActivityIndicator } from 'react-native';
+import { Button, StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 const url = "https://reactnative.dev/movies.json";
 
