408894ede1f268b650a1fb52d715ec07e877f634
Zia Uddin Chowdhury <rifatxia9@gmail.com>
2023-07-26 03:14:33 +0600

    node modules updated

--- instructions.bat
+++ instructions.bat
@@ -2,3 +2,4 @@ start %windir%\System32\cmd.exe "/K" D:\ProgramData\Anaconda3\Scripts\activate.b
 cd C:\Users\RifatXia\Desktop\CSE499 Project\CSE499-Project\frontend
 conda activate myenv
 streamlit run app.py
+
