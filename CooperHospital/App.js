29867addb02979d177b42a76f1c8178600a6196e
Zia Uddin Chowdhury <rifatxia9@gmail.com>
2023-07-26 21:15:47 +0600

    django admin setup

--- CooperHospital/App.js
+++ CooperHospital/App.js
@@ -1,16 +1,77 @@
-import React, { useState, useEffect } from 'react';
-import { Button, StyleSheet, Text, View, SafeAreaView } from 'react-native';
+// import React, { useState, useEffect } from 'react';
+// import { Button, StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
-const url = "https://reactnative.dev/movies.json";
+// const url = "https://reactnative.dev/movies.json";
 
-const App = () => {
+// const App = () => {
 
-    return (
-        <SafeAreaView style={ styles.body }>
-            <Text style={ styles.text }>Hello Zia</Text>
-        </SafeAreaView>
-    );
-};
+//     return (
+//         <SafeAreaView style={ styles.body }>
+//             <Text style={ styles.text }>Hello Zia</Text>
+//         </SafeAreaView>
+//     );
+// };
+
+// export default App;
+
+import { Text, View, Image } from 'react-native'
+import React, { Component } from 'react'
+import { GoogleSignin, GoogleSigninButton } from '@react-native-community/google-signin'
+
+// get the google client ID from Google OAuth 
+GoogleSignin.configure({
+    webClientId: '248229658628-cd6s6cauehhb8qs9a8m2uk1t88rmhkn5.apps.googleusercontent.com',
+    offlineAccess: true
+})
+
+export class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            userGoogleInfo: {},
+            loaded: false
+        };
+    }
+
+    // signin function to fetch the user info 
+    signIn = async() => {
+        try{
+            await GoogleSignin.hasPlayServices()
+            const userInfo = await GoogleSignin.signIn();
+            this.state({
+                userGoogleInfo: userInfo,
+                loaded: true
+            })
+        }
+        catch(error){
+            console.log(error.message);
+        }
+    }
+    
+    render() {
+        return (
+            <View>
+                <GoogleSigninButton
+                    onPress={ this.signIn }
+                    size={ GoogleSigninButton.Size.Wide }
+                    color={ GoogleSigninButton.Color.Dark }
+                    style={ styles.sign_in }
+                />
+                {
+                    this.state.loaded ? (
+                        <View>
+                            <Text>this.state.userGoogleInfo.user.name</Text>
+                            <Text>this.state.userGoogleInfo.user.email</Text>
+                            <Image style={ styles.sign_in } source={{ uri:this.state.userGoogleInfo.user.photo }} ></Image>
+                        </View>
+                    ) : (
+                        <Text>Not Signed In</Text>
+                    )
+                }
+            </View>
+        )
+    }
+}
 
 const styles = StyleSheet.create({
     body:{
@@ -19,12 +80,16 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
     },
-    // color and text of the font 
+    // color and text of the font
     text:{
         color:'#ffffff',
         fontSize: 20,
         fontFamily: 'Italic',
-    }
+    },
+    sign_in:{
+        width:100,
+        height:100,
+    },
 });
 
-export default App;
+export default App
