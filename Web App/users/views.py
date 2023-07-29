88e5f8674aeb42938ff7c7a6a4d164c60cc81f82
Zia Uddin Chowdhury <rifatxia9@gmail.com>
2023-07-30 00:25:20 +0600

    static css files fixed

--- Web App/users/views.py
+++ Web App/users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'homepage/homepage.html')
 
 def logout_view(request):
     logout(request)
