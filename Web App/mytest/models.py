2e76987110ec39b0f673aa55cd5945b4613586a6
Nafisa Prome <aymannafisapromi390@gmail.com>
2023-07-29 20:42:30 +0600

    test

--- Web App/mytest/models.py
+++ Web App/mytest/models.py
@@ -3,9 +3,9 @@ from datetime import date
 
 class Person(models.Model):
     name = models.CharField(max_length=100)
-    dob = models.DateField(default=date.today)
-    age = models.IntegerField(default=1)
-    gen = models.CharField(max_length=6,default='Male')
-    email = models.EmailField(default='sky@gmail.com')
-    phn = models.IntegerField(default=1)
+    dob = models.DateField()
+    age = models.IntegerField()
+    gen = models.CharField(max_length=6)
+    email = models.EmailField()
+    phn = models.IntegerField()
 
