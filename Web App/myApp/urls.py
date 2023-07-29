5c72fc79ea2b74f72605a0084ce00f322502034a
Zia Uddin Chowdhury <rifatxia9@gmail.com>
2023-07-30 04:37:30 +0600

    static hndling of the JS files

--- Web App/myApp/urls.py
+++ Web App/myApp/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('static/<path:path>', serve, {'document_root': settings.STATIC_ROOT}),
     path('hospital/', include('hospital.urls')),
     path('', include('users.urls')),
 ]
