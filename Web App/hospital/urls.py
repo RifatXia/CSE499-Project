b9f199d9affc30ae451ba13d047caa7dbc6d1436
Nafisa Prome <aymannafisapromi390@gmail.com>
2023-07-30 21:04:13 +0600

    person details fetch

--- Web App/hospital/urls.py
+++ Web App/hospital/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('add_person/', views.add_person, name='add_person'),
     path('success/', views.success, name='success'),
     path('login/', views.login, name='login'),
+    path('person/<int:user_id>/', views.get_person, name='get_person'),
 ]
