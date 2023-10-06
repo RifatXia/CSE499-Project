a69b3b7e6d84ce46fd9c18fb9e11fa0cb3ada5bf
Nafisa Prome <aymannafisapromi390@gmail.com>
2023-10-06 19:29:34 +0600

    keypoints match with dataset

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import numpy as np
 import sklearn
 import spacy
 from spacy.matcher import PhraseMatcher
+import pandas as pd
 
 # setting the page configuration 
 st.set_page_config(page_title="Disease Detection", page_icon="", layout="wide")
@@ -47,17 +48,19 @@ user_input = [value for value in symptoms_map.values()]
 if st.button("Get Disease"):
     predicted_disease = model.predict([user_input])
     st.title(predicted_disease)
-    
+
 # Load spaCy model
 nlp = spacy.load('en_core_web_sm')
+
+dataset = pd.read_csv('dataset\processed_data_2.csv')
 
 # Create a text input box
 text = st.text_input("Enter your text:")
 
 #Create a button to display the key points:
 if st.button("Show Key Points"):
-    # Process the text with spaCy
-    doc = nlp(text)
+# Process the text with spaCy
+   doc = nlp(text)
 
 # Extract key points
 key_points = [token.text for token in doc if token.pos_ == "NOUN" or token.pos_ == "ADJ"]
@@ -65,5 +68,18 @@ key_points = [token.text for token in doc if token.pos_ == "NOUN" or token.pos_ 
 # Display the key points
 for point in key_points:
     st.write(point)
+
+# Check if each key point is present in the dataset
+    matching_diseases = set()
+
+    # Iterate through symptom columns
+    for col in dataset.columns:
+        if col.startswith('Symptom_'):
+            # Handle NaN values by filling them with an empty string
+            matching_rows = dataset[dataset[col].fillna('').str.contains('|'.join(key_points), case=False, regex=True)]
+            matching_diseases.update(matching_rows['Disease'])
+
+    # Display matching diseases
+    st.write("Matching Diseases:", list(matching_diseases))
 
 
