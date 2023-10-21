6e6e99beaba31e3aba1442e2babb28644ec1646f
Zia Uddin Chowdhury <rifatxia9@gmail.com>
2023-10-21 17:30:36 +0600

    disease categorization and redirect to doctors

--- app1.py
+++ app1.py
@@ -21,9 +21,31 @@ dataset = pd.read_csv('dataset/merged_data.csv')
 # Initialize user input
 user_input = st.text_input("Describe your symptoms:")
 
-if st.button("Redirect"):
-    st.markdown('<meta http-equiv="refresh" content="0; URL=http://127.0.0.1:8000/hospital/get_doctor/uro/">', unsafe_allow_html=True)
+disease_name = 'Bipolar Disorder'
 
+categories = {
+    'uro': ['Urinary tract infection', 'Kidney Disease', 'Bladder Disorder'],
+    'psy': ['Bipolar Disorder', 'Schizophrenia', 'Delusion', 'Manic Disorder', 'Depressive Disorder', 'Paranoia'],
+    'cancer': ['Melanoma', 'Prostate Cancer', 'Carcinoma of Lung', 'Breast Infiltrating Ductal Carcinoma'],
+    'gas': ['Gastroenteritis', 'Peptic ulcer disease', 'Duodenal ulcer', 'Aphthous Stomatitis', 'Gastritis', 'Gastrointestinal Hemorrhage'],
+    'skin': ['Psoriasis', 'Acne', 'Fungal infection'],
+    'med': ['Hypertension', 'Diabetes', 'Hypoglycemia', 'Chronic Kidney Failure', 'Obesity', 'Hyperlipidemia'],
+    'gyn': ['Cervical spondylosis', 'Polycystic Ovary Syndrome', 'Endometriosis', 'Cervical Dysplasia', 'Leiomyoma', 'Menstruation Disorders'],
+    'car': ['Myocardial Infarction', 'Atherosclerosis', 'Ischemia', 'Coronary Heart Disease', 'Angina Pectoris'],
+    'art': ['Arthritis', 'Joint Pain', 'Spondylolisthesis', 'Arthropathy', 'Osteoarthritis', 'Fibromyalgia'],
+    'ear': ['Ear Infection', 'Tinnitus', 'Hearing Problem', 'Otitis Media', 'Meniere Disease', 'Acoustic Neuroma'],
+    'misc': ['Bacteremia', 'Parkinson Disease', 'Hyperglycemia', 'Failure Heart', 'Obesity Morbid', 'Confusion', 'Sickle Cell Anemia', 'Tonic-Clonic Seizures', 'Herniahiatal', 'Osteoarthristis', 'Primary Carcinoma Of The Liver Cells', 'Migraine Disorders', 'Alcoholic hepatitis', 'Hypercholesterolemia', 'Obesity', 'Upper Respiratory Infection', 'Neutropenia', 'Ileus', 'Cholecystitis', 'Lymphatic Diseases', 'Anxiety State', 'Pericardial Effusion Body Substance', 'Personality Disorder', 'AIDS', 'Failure Kidney', 'Malaria', 'Hyperlipidemia', 'Hypertension ', 'Schizophrenia', 'Influenza', 'Dementia', 'Cervical spondylosis', 'HIV Infections', 'Pancreatitis', 'Depressive Disorder', 'Hypoglycemia', 'Hemorrhoids', 'Hepatitis E', 'Insufficiency Renal', 'Osteoporosis', 'Gastroenteritis', 'Endocarditis', 'Transient Ischemic Attack', 'Pneumocystiscariniipneumonia', 'Cirrhosis', 'Ischemia', 'Deglutition Disorder', 'Lymphoma', 'Diverticulitis', 'Common Cold', 'Delusion', 'Drug Reaction', 'Stenosis Aortic Valve', 'Primary Malignant Neoplasm', 'Melanoma', 'hepatitis A', 'Hemiparesis', 'Jaundice', 'Gastroesophageal Reflux Disease', 'Tuberculosis', 'Manic Disorder', 'Myocardial Infarction', 'Colitis', 'Bronchial Asthma', 'Failure Heart Congestive', 'Accidentcerebrovascular', 'Pancytopenia', 'Pyelonephritis', 'Carcinoma', 'Mitral Valve Insufficiency', 'Dimorphic hemmorhoids(piles)', 'Hyperthyroidism', 'Oralcandidiasis', 'Pneumothorax', 'Asthma', 'Hyperbilirubinemia', 'Benign Prostatic Hypertrophy', 'Dengue', 'Thrombocytopaenia', "Alzheimer'S Disease", '(vertigo) Paroymsal  Positional Vertigo', 'Arthritis', 'Neuropathy', 'Hypertension Pulmonary', 'Peripheral Vascular Disease', 'Chicken pox', 'Carcinoma Of Lung', 'Cardiomyopathy', 'Emphysema Pulmonary', 'Hypertensive Disease', 'Hypothyroidism', 'Diabetes', 'Varicose veins', 'Gastritis', 'Peptic ulcer diseae', 'Psychotic Disorder', 'Typhoid', 'Chronic Kidney Failure', 'Tricuspid Valve Insufficiency', 'Cellulitis', 'Biliary Calculus', 'Kidney Failure Acute', 'Hernia', 'Neoplasm', 'Encephalopathy', 'Dehydration', 'Infection', 'Diabetes ', 'Adenocarcinoma', 'Paralysis (brain hemorrhage)', 'Hepatitis C', 'Embolism Pulmonary', 'Ketoacidosis Diabetic', 'Degenerativepolyarthritis', 'Malignantneoplasms', 'Hepatitis B', 'Infection Urinary Tract', 'Bronchitis', 'Fibroid Tumor', 'GERD', 'Diverticulosis', 'Neoplasm Metastasis', 'Delirium', 'Pneumonia Aspiration', 'Glaucoma', 'Gout', 'Osteomyelitis', 'Hepatitis D', 'Thrombus', 'Incontinence', 'Chronic Alcoholic Intoxication', 'Kidney Disease', 'Overload Fluid', 'Hepatitis', 'Edema Pulmonary', 'Allergy', 'Chronic cholestasis', 'Pneumonia', 'Paroxysmaldyspnea', 'Dependence', 'Tachycardia Sinus', 'Affect Labile', 'Decubitus Ulcer', 'Exanthema', 'Migraine', 'Carcinoma Colon', 'Spasm Bronchial', 'Aphasia', 'Deep Vein Thrombosis', 'Anemia', 'Respiratory Failure', 'Carcinoma Prostate', 'Chronic Obstructive Airway Disease', 'Suicide Attempt', 'Epilepsy', 'Impetigo', 'Coronary Heart Disease', 'Sepsis (Invertebrate)', 'Carcinoma Breast', 'Ulcer Peptic', 'Adhesion', 'Paranoia', 'Heart attack'],
+}
+
+keyword = ""
+for ind in categories:
+    for name in categories[ind]:
+        if name == disease_name:
+            keyword = ind 
+
+if st.button("Get appointment"):
+    url = f'http://127.0.0.1:8000/hospital/get_doctor/{keyword}/'
+    st.markdown(f'<meta http-equiv="refresh" content="0; URL={url}">', unsafe_allow_html=True)
 
 # while user_input:
 #     # Process the text with spaCy
