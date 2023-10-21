f89b4586143835331bbc1b416d4c28a3c1a7babc
Zia Uddin Chowdhury <rifatxia9@gmail.com>
2023-10-21 16:37:45 +0600

    redirecting from streamlit to html

--- Web App/hospital/urls.py
+++ Web App/hospital/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('person/<int:user_id>/', views.get_person, name='get_person'),
     path('doctor_list/', views.doctor_list, name='doctor_list'),
-    path('get_doctor/<str:specialization>/', views.get_doctor, name='get_doctor'),
+    path('get_doctor/<str:keyword>/', views.get_doctor, name='get_doctor'),
 
     path('about/', views.about, name='about'),
 ]
--- app1.py
+++ app1.py
@@ -21,41 +21,27 @@ dataset = pd.read_csv('dataset/merged_data.csv')
 # Initialize user input
 user_input = st.text_input("Describe your symptoms:")
 
-while user_input:
-    # Process the text with spaCy
-    doc = nlp(user_input)
+if st.button("Redirect"):
+    st.markdown('<meta http-equiv="refresh" content="0; URL=http://127.0.0.1:8000/hospital/get_doctor/uro/">', unsafe_allow_html=True)
 
-    # Extract key points
-    key_points = [token.text for token in doc if token.pos_ == "NOUN" or token.pos_ == "ADJ"]
 
-    # Display the key points
-    # st.write("Extracted Key Points:")
-    # st.write(key_points)
+# while user_input:
+#     # Process the text with spaCy
+#     doc = nlp(user_input)
 
-    # Check if each key point is present in the dataset
-    matching_diseases = set()
+#     # Extract key points
+#     key_points = [token.text for token in doc if token.pos_ == "NOUN" or token.pos_ == "ADJ"]
 
-    # Iterate through symptom columns
-    symptom_columns = [f'Symptom_{i}' for i in range(1, 29)]
-    for col in symptom_columns:
-        matching_rows = dataset[dataset[col].fillna('').str.contains('|'.join(key_points), case=False, regex=True)]
-        if not matching_rows.empty:
-            matching_diseases.update(matching_rows['Disease'])
+#     # Display the key points
+#     # st.write("Extracted Key Points:")
+#     # st.write(key_points)
 
-    # st.write("Matching Diseases:", list(matching_diseases))
+#     # Check if each key point is present in the dataset
+#     matching_diseases = set()
 
-    # Ask follow-up questions for each identified disease
-    for disease in matching_diseases:
-        st.write(f"Follow-up questions for {disease}:")
-
-        # Assuming symptoms are stored in columns like 'Symptom_1', 'Symptom_2', ..., 'Symptom_29'
-        symptoms_in_disease = [dataset.loc[dataset['Disease'] == disease, f'Symptom_{i}'].values[0] for i in range(1, 29)]
-
-        # Remove NaN values from symptoms_in_disease
-        symptoms_in_disease = [symptom for symptom in symptoms_in_disease if pd.notna(symptom)]
-
-        for i in range(0, len(symptoms_in_disease)):
-            symptom = symptoms_in_disease[i]
-            unique_key = f"{disease}_{symptom.lower()}"
-            user_input = st.text_input(f"Do you have {symptom.lower()}? (yes/no)", key=unique_key)
-      
+#     # Iterate through symptom columns
+#     symptom_columns = [f'Symptom_{i}' for i in range(1, 29)]
+#     for col in symptom_columns:
+#         matching_rows = dataset[dataset[col].fillna('').str.contains('|'.join(key_points), case=False, regex=True)]
+#         if not matching_rows.empty:
+#             matching_diseases.update(matching_rows['Disease'])  
