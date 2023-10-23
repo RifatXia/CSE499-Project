c33ccf778140f2696594b41e1526d52acfdc979e
Nafisa Prome <aymannafisapromi390@gmail.com>
2023-10-23 20:48:52 +0600

    user authentication

--- Web App/hospital/urls.py
+++ Web App/hospital/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('add_person/', views.add_person, name='add_person'),
     path('success/', views.success, name='success'),
-    # path('login/', views.login, name='login'),
+    path('login/', views.login, name='login'),
     path('person/<int:user_id>/', views.get_person, name='get_person'),
     path('doctor_list/', views.doctor_list, name='doctor_list'),
     path('get_doctor/<str:keyword>/', views.get_doctor, name='get_doctor'),
